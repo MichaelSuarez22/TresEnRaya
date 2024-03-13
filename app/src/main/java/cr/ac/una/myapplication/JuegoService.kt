9a37149ec9a9e91eea727524f5cbad2104a8788b
MichaelSuarez22 <maiksc84@gmail.com>
2024-03-12 22:48:13 -0600

    cambios

--- app/src/main/java/cr/ac/una/myapplication/JuegoService.kt
+++ app/src/main/java/cr/ac/una/myapplication/JuegoService.kt
@@ -10,19 +10,84 @@ class JuegoService  {
     fun jugada(x:Int, y:Int): String{
         matriz[x][y]= figura
         if (figura == 'X') figura = 'O' else figura = 'X'
-        return "Turno de "+ figura
+
+        val estadoJuego = obtenerEstadoJuego()
+        if (estadoJuego != "") {
+            return estadoJuego // Si hay un ganador, devuelve el mensaje correspondiente.
+        }
+
+        return "Turno de $figura"
     }
+
     fun inicializar(){
-        matriz[0][0] = ' '
-        matriz[0][1] = ' '
-        matriz[0][2] = ' '
-        matriz[1][0] = ' '
-        matriz[1][1] = ' '
-        matriz[1][2] = ' '
-        matriz[2][0] = ' '
-        matriz[2][1] = ' '
-        matriz[2][2] = ' '
-
-
+        for (i in 0 until 3) {
+            for (j in 0 until 3) {
+                matriz[i][j] = ' '
+            }
+        }
     }
+
+    fun obtenerEstadoJuego():String {
+
+        for (i in 0 until 3) {
+            if (matriz[i][0] == matriz[i][1] && matriz[i][1] == matriz[i][2] && matriz[i][0] != ' ') {
+                return "${matriz[i][0]} gana"
+            }
+            if (matriz[0][i] == matriz[1][i] && matriz[1][i] == matriz[2][i] && matriz[0][i] != ' ') {
+                return "${matriz[0][i]} gana"
+            }
+        }
+
+
+        if (matriz[0][0] == matriz[1][1] && matriz[1][1] == matriz[2][2] && matriz[0][0] != ' ') {
+            return "${matriz[0][0]} gana"
+        }
+        if (matriz[0][2] == matriz[1][1] && matriz[1][1] == matriz[2][0] && matriz[0][2] != ' ') {
+            return "${matriz[0][2]} gana"
+        }
+
+        // Verificar empate
+        if (esEmpate()) {
+            return "Empate"
+        }
+
+        // El juego sigue
+        return "Sigue ${figura}"
+    }
+
+    private fun esEmpate(): Boolean {
+        for (i in 0 until 3) {
+            for (j in 0 until 3) {
+                if (matriz[i][j] == ' ') {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    fun hayGanador(): Boolean {
+        val estadoJuego = obtenerEstadoJuego()
+        return estadoJuego.startsWith("¡Ganador:")
+    }
+
+    fun obtenerLineaGanadora(): List<Pair<Int, Int>>? {
+        val estadoJuego = obtenerEstadoJuego()
+        if (hayGanador()) {
+            val partes = estadoJuego.split(" ")
+            val fila = partes[2].toInt()
+            val columna = partes[3].toInt()
+
+            // Devolver las posiciones de la línea ganadora
+            return when (partes[1]) {
+                "fila" -> listOf(Pair(fila, 0), Pair(fila, 1), Pair(fila, 2))
+                "columna" -> listOf(Pair(0, columna), Pair(1, columna), Pair(2, columna))
+                "diagonal" -> listOf(Pair(0, 0), Pair(1, 1), Pair(2, 2))
+                "antidiagonal" -> listOf(Pair(0, 2), Pair(1, 1), Pair(2, 0))
+                else -> null
+            }
+        }
+        return null
+    }
+
 }
--- app/src/main/java/cr/ac/una/myapplication/MainActivity.kt
+++ app/src/main/java/cr/ac/una/myapplication/MainActivity.kt
@@ -1,30 +1,32 @@
 package cr.ac.una.myapplication
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.TextView
-import androidx.fragment.app.DialogFragment
-
 
 class MainActivity : AppCompatActivity() {
-    lateinit var jugar :Button
+    lateinit var jugar: Button
     lateinit var pos1: ImageButton
     lateinit var pos2: ImageButton
     lateinit var pos3: ImageButton
     lateinit var pos4: ImageButton
     lateinit var pos5: ImageButton
     lateinit var pos6: ImageButton
+    lateinit var pos7: ImageButton
+    lateinit var pos8: ImageButton
+    lateinit var pos9: ImageButton
     var juegoService = JuegoService()
 
-
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         jugar = findViewById(R.id.jugar)
+        jugar.isEnabled = true
 
         pos1 = findViewById(R.id.pos1)
         pos2 = findViewById(R.id.pos2)
@@ -32,52 +34,224 @@ class MainActivity : AppCompatActivity() {
         pos4 = findViewById(R.id.pos4)
         pos5 = findViewById(R.id.pos5)
         pos6 = findViewById(R.id.pos6)
+        pos7 = findViewById(R.id.pos7)
+        pos8 = findViewById(R.id.pos8)
+        pos9 = findViewById(R.id.pos9)
 
         jugar.setOnClickListener() {
             enableDisableButton()
+            juegoService.inicializar()
         }
-        pos1.setOnClickListener(){
-            seleccionafigura(it as ImageButton)
-            muestraDialogo (juegoService.jugada(0,0))
+
+        pos1.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(0, 0)
+            val estadoJuego = juegoService.obtenerEstadoJuego()
+
+
+            muestraDialogo(resultado)
 
         }
-        pos2.setOnClickListener(){
-            seleccionafigura(it as ImageButton)
-            muestraDialogo (juegoService.jugada(0,0))
+
+        pos2.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(0, 1)
+            val estadoJuego = juegoService.obtenerEstadoJuego()
+
+            muestraDialogo(resultado)
 
         }
+
+        pos3.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(0, 2)
+
+            muestraDialogo(resultado)
+
+        }
+
+        pos4.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(1, 0)
+
+            muestraDialogo(resultado)
+
+        }
+
+        pos5.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(1, 1)
+
+            muestraDialogo(resultado)
+
+        }
+
+        pos6.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(1, 2)
+
+            muestraDialogo(resultado)
+
+        }
+
+        pos7.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(2, 0)
+
+            muestraDialogo(resultado)
+
+        }
+
+        pos8.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(2, 1)
+
+            muestraDialogo(resultado)
+
+        }
+
+        pos9.setOnClickListener() {
+            seleccionaFigura(it as ImageButton)
+            val resultado = juegoService.jugada(2, 2)
+            val estadoJuego = juegoService.obtenerEstadoJuego()
+            if (estadoJuego.equals("Empate") or estadoJuego.equals("gana")) {
+                verificarGanador(resultado)
+            } else {
+                muestraDialogo(resultado)
+            }
+        }
+
         enableDisableButton()
     }
 
-    private fun enableDisableButton(){
-        pos1.isEnabled =  !pos1.isEnabled
-        pos2.isEnabled =  !pos2.isEnabled
-        pos3.isEnabled =  !pos3.isEnabled
-        pos4.isEnabled =  !pos4.isEnabled
-        pos5.isEnabled =  !pos5.isEnabled
-        pos6.isEnabled =  !pos6.isEnabled
+    private fun enableDisableButton() {
+        pos1.isEnabled = !pos1.isEnabled
+        pos2.isEnabled = !pos2.isEnabled
+        pos3.isEnabled = !pos3.isEnabled
+        pos4.isEnabled = !pos4.isEnabled
+        pos5.isEnabled = !pos5.isEnabled
+        pos6.isEnabled = !pos6.isEnabled
+        pos7.isEnabled = !pos7.isEnabled
+        pos8.isEnabled = !pos8.isEnabled
+        pos9.isEnabled = !pos9.isEnabled
     }
 
-    private fun seleccionafigura(imageButton: ImageButton){
+    private fun seleccionaFigura(imageButton: ImageButton) {
         if (juegoService.figura == 'O')
             imageButton.setBackgroundResource(R.drawable.circulo)
         else
             imageButton.setBackgroundResource(R.drawable.cruz)
-        imageButton.isEnabled=false
+        imageButton.isEnabled = false
     }
-    private fun muestraDialogo(mensaje: String){
+
+    private fun muestraDialogo(mensaje: String) {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder
             .setMessage("Aviso")
             .setTitle(mensaje)
             .setPositiveButton("¡Entendido!") { dialog, which ->
-                // Do something.
-            }/*
-            .setNegativeButton("Negative") { dialog, which ->
-                // Do something else.
-            }*/
+                val resultado = juegoService.obtenerEstadoJuego()
+
+                if (resultado.startsWith("¡Ganador:") || resultado == "Empate") {
+                    val lineaGanadora = juegoService.obtenerLineaGanadora()
+
+                    val felicitaciones = if (resultado.startsWith("¡Ganador:")) {
+                        "$resultado\n¡Felicidades, ganador!"
+                    } else {
+                        resultado
+                    }
+
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+                    builder.setMessage(felicitaciones)
+                        .setTitle("¡Fin del juego!")
+                        .setPositiveButton("Reiniciar") { dialog, which ->
+                            enableDisableButton()
+                            reiniciarJuego()
+                        }
+
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
+
+                    // Resaltar la línea ganadora
+                    if (lineaGanadora != null) {
+                        resaltarLineaGanadora(lineaGanadora)
+                    }
+                }
+            }
 
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
+
+    private fun verificarGanador(resultado: String) {
+        if (juegoService.hayGanador()) {
+            val lineaGanadora = juegoService.obtenerLineaGanadora()
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            val mensaje = "$resultado\n¡Felicidades, ganador!"
+
+            builder.setMessage(mensaje)
+                .setTitle("¡Fin del juego!")
+                .setPositiveButton("Reiniciar") { dialog, which ->
+                    enableDisableButton()
+                    reiniciarJuego()
+                }
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+
+            // Resaltar la línea ganadora
+            if (lineaGanadora != null) {
+                resaltarLineaGanadora(lineaGanadora)
+            }
+        }
+    }
+
+    private fun resaltarLineaGanadora(posiciones: List<Pair<Int, Int>>) {
+        val ganador = juegoService.figura
+
+        for ((fila, columna) in posiciones) {
+            when (fila * 3 + columna) {
+                0 -> pos1.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                1 -> pos2.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                2 -> pos3.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                3 -> pos4.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                4 -> pos5.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                5 -> pos6.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                6 -> pos7.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                7 -> pos8.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+                8 -> pos9.setImageResource(if (ganador == 'O') R.drawable.circulo_rojo else R.drawable.cruz_roja)
+            }
+        }
+
+        // Desactivar botones después de resaltar la línea ganadora
+        enableDisableButton()
+    }
+
+    private fun reiniciarJuego() {
+        juegoService.inicializar()
+
+        pos1.isEnabled = false
+        pos2.isEnabled = false
+        pos3.isEnabled = false
+        pos4.isEnabled = false
+        pos5.isEnabled = false
+        pos6.isEnabled = false
+        pos7.isEnabled = false
+        pos8.isEnabled = false
+        pos9.isEnabled = false
+
+        pos1.setBackgroundResource(R.drawable.limpio)
+        pos2.setBackgroundResource(R.drawable.limpio)
+        pos3.setBackgroundResource(R.drawable.limpio)
+        pos4.setBackgroundResource(R.drawable.limpio)
+        pos5.setBackgroundResource(R.drawable.limpio)
+        pos6.setBackgroundResource(R.drawable.limpio)
+        pos7.setBackgroundResource(R.drawable.limpio)
+        pos8.setBackgroundResource(R.drawable.limpio)
+        pos9.setBackgroundResource(R.drawable.limpio)
+
+        enableDisableButton()
+    }
 }
+
